7ad8a6b939a26e00dc9b3d837bc7364eca36ca12
LounsburyA <94415015+LounsburyA@users.noreply.github.com>
2022-06-02 10:41:52 -0500

    Initialize feature-form-line-break

---
+++ server/routes/profile.router.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const { user } = require('pg/lib/defaults');
+
+
+
+
+// get specific users profile information for profile page profiles table joined to user table
+
+router.get('/:id', (req, res) => {
+  const query = `SELECT "user".username,
+  "profiles".id,
+  "profiles".profile_picture,
+  "profiles".device, 
+  "profiles".device_settings,
+  "profiles".injury_level,
+  "profiles".aisa_level,
+  "profiles".time_since_injury,
+  "profiles".baseline,
+  "profiles".improvements,
+  "profiles".location,
+  "profiles".job_title,
+  "profiles".company,
+  "profiles".about_me,
+  "profiles".contact,
+  "profiles".biological_gender,
+  "profiles".age,
+  "profiles".pronouns,
+  "profiles".height,
+  "profiles".weight,
+  "profiles".medical_conditions,
+  "profiles".public, 
+  "profiles".user_id
+  FROM "profiles"
+  JOIN "user" ON "profiles".user_id = "user".id
+  WHERE "profiles".user_id = $1;`
+  
+  pool.query(query, [req.params.id])
+// security check here for user profile information 2 is private, 1 only logged in users can see, 0 public
+    .then((results) => {
+      if (results.rows[0].public === 0) {
+        res.send(results.rows)
+      } else if (results.rows[0].public === 1 && req.user.id) {
+        res.send(results.rows)
+      } else if (results.rows[0].public === 2 && req.user.id === results.rows[0].user_id) {
+        res.send(results.rows)
+      } else { res.sendStatus(403) }
+    })
+
+
+    .catch((err) => {
+      console.log('Error in PROFILE GET', err)
+    })
+
+});
+
+router.put('/:id', rejectUnauthenticated, (req, res) => {
+
+
+  const update = req.body
+  const query = `UPDATE "profiles" SET  "profile_picture" = $1, "device" = $2, "device_settings" = $3,
+  "injury_level" = $4, "aisa_level" = $5, "time_since_injury" = $6, "baseline" = $7,
+  "improvements" = $8,"location" = $9,"job_title" = $10,"company" = $11,"about_me" = $12,
+  "contact" = $13,"biological_gender" = $14,"age" = $15,"pronouns" = $16,"height" = $17,
+  "weight" = $18,  "medical_conditions" = $19, "public" = $20 WHERE "user_id" = $21;`;
+
+  const values = [update.profile_picture, update.device, update.device_settings, update.injury_level, update.aisa_level,
+  update.time_since_injury, update.baseline, update.improvements, update.location, update.job_title, update.company, update.about_me,
+  update.contact, update.biological_gender, update.age, update.pronouns, update.height, update.weight, update.medical_conditions, update.public, req.user.id]
+  pool.query(query, values)
+    .then(result => {
+      res.sendStatus(200);
+    }).catch(error => {
+      console.log('error', error);
+
+    })
+
+});
+
+router.delete('/', rejectUnauthenticated, (req, res) => {
+  const id = req.user.id;
+  console.log('DELETE id:', id);
+
+  const query = `DELETE FROM "user" WHERE "id" = $1`;
+  values = [id];
+  pool.query(query, values)
+    .then(() => { res.sendStatus(200); })
+    .catch((err) => {
+      console.log('Error in Profile(user) DELETE', err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
---
+++ src/components/_PostListItem/PostListItem.jsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import './PostListItem.css'
+
+// imports for MUI v5
+import {
+    Box,
+    Container,
+    Paper,
+    Select,
+    MenuItem,
+    InputLabel,
+    Button,
+    Modal,
+    Typography,
+    TextField,
+    FormControl,
+    Avatar,
+    ListItem,
+    List,
+    Divider,
+    ListItemAvatar,
+    ListItemText,
+    Chip
+} from '@mui/material';
+
+function PostListItem({ post }) {
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const user = useSelector(store => store.user);
+    const outcomesList = useSelector(store => store.outcomesListReducer);
+
+    const handleClick = () => {
+        history.push('')
+    }
+
+    const postLengthCheck = () => {
+        if (post.post.length > 150) {
+            return post.post.substring(0, 150) + '.....'
+        } else {
+            return post.post
+        }
+    }
+
+    return (
+        <Box
+            component={Paper}
+            sx={{
+                padding: '15px',
+                borderRadius: '7px',
+                border: '1px solid black',
+                boxShadow: 10,
+                marginBottom: '15px',
+                minWidth: '60vw',
+                maxWidth: '60vw',
+            }}>
+            <ListItem alignItems="flex-start">
+                <ListItemAvatar
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        paddingRight: 3
+                    }}>
+                    <Avatar 
+                        className="hoverForPointer" 
+                        alt="Profile Picture" 
+                        src={post.profile_picture} 
+                        onClick={() => { history.push(`/profile/${post.user_id}`) }} 
+                    />
+                </ListItemAvatar>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        width: '100%'
+                    }}
+                >
+                    <ListItemText
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between'
+                        }}
+                        primary={
+                            <Box
+                                className="hoverForPointer"
+                                onClick={() => { history.push(`/postDetail/${post.id}`) }}
+                            >
+                                <Typography
+                                    component="span"
+                                    variant="h4"
+                                    color="text.primary"
+                                >
+                                    {post.title}
+                                </Typography>
+                                <br />
+                                <Typography
+                                    component="span"
+                                    variant="body1"
+                                    color="text.primary"
+                                    sx={{ whiteSpace: "pre-line" }}
+                                >
+                                    {postLengthCheck()}
+                                </Typography>
+                            </Box>
+                        }
+                        secondary={
+                            <React.Fragment>
+                                <Box sx={{
+                                    marginTop: '10px'
+                                }}>
+                                    Posted by: <a 
+                                                onClick={() => { history.push(`/profile/${post.user_id}`) }}>{post.username}
+                                                </a> {post.date} @ {post.time}
+                                </Box>
+
+                                <br />
+                                <Chip
+                                    label={outcomesList[post.outcome_id - 1]?.outcome}
+                                    variant="outlined"
+                                    sx={{
+                                        bgcolor: '#5b7495',
+                                        color: 'white',
+                                        border: '1px solid #5b7495'
+                                    }}
+                                />
+                            </React.Fragment>
+                        }
+                    />
+                </Box>
+            </ListItem>
+        </Box>
+    );
+}
+
+export default PostListItem;
